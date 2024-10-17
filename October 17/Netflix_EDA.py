21535f5b3595198dbcd0761feea9ee0fa1aa5c79
Nallaka Naga Durga DhanaLakshmi <62500693+dhana-nallaka@users.noreply.github.com>
2024-10-17 13:05:38 -0500

    Update Netflix_EDA.py
    The main aim of the project is to investigate the netflix movie data and do the exploratory data analysis.
    
    This data analysis answers the questions
    
    What was the most frequent movie duration in the 1990s?
    A movie is considered short if it is less than 90 minutes. Count the number of short action movies released in the 1990s

--- October 17/Netflix_EDA.py
+++ October 17/Netflix_EDA.py
@@ -1,1 +1,15 @@
+# Importing pandas and matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Read in the Netflix CSV as a DataFrame
+netflix_df = pd.read_csv("netflix_data.csv")
+duration=netflix_df['duration'].mode()[0]
+short_movie_count = netflix_df[
+    (netflix_df['duration'] < 90) & 
+    (netflix_df['genre'] == 'Action') & 
+    (netflix_df['release_year'] >= 1990) & 
+    (netflix_df['release_year'] < 2000)
+].shape[0]
+
+print(short_movie_count)
